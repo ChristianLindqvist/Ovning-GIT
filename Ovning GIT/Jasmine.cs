5aefd1ceddec9deb70d2e6f571818f95a83ae815
Jasmine Linde <256299221+jasminecommits@users.noreply.github.com>
2026-07-02 11:30:55 +0200

    Added a new class

---
+++ Ovning GIT/Jasmine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning_GIT
+{
+    internal class Jasmine
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hi, my name is Jasmine");
+        }
+    }
+}
