af6fe2a1cc2405e4aea9ba3011b5cee7986eb020
Naima Korane <naima.korane@gmail.com>
2026-07-02 12:00:43 +0200

    Update Naima class

---
+++ Ovning GIT/Naima.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ovning_GIT
+{
+    public class Naima
+    {
+        public Naima()
+        {
+            Console.WriteLine("Hello, from Naima!");
+        }
+    }
+}
